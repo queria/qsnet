b2b9a6427e0503370eb131b81afda69c60dc1acb
Queria Sa-Tas <queria@sa-tas.net>
2012-10-05 12:03:25 +0200

    rozdeleni arp view a scrollbar u mac-notes

--- public/mac.js
+++ public/mac.js
@@ -1,5 +1,6 @@
 
 $(document).ready( function () {
+  $('#macNotes .inner').css('height', ($(window).height() - 220)+'px');
   $('.mac').mouseenter( function () {
     $('.mac[data-mac='+$(this).attr('data-mac')+']').addClass('macOver');
   });
